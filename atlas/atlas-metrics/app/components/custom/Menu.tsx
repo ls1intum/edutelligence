378e01297dc567dbd12b7e7f361145f239e83d06
Maximilian Anzinger <anzinger@in.tum.de>
2024-11-28 16:14:49 +0100

    create service select

--- atlas/atlas-metrics/app/components/custom/Menu.tsx
+++ atlas/atlas-metrics/app/components/custom/Menu.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { DateRangePicker } from "@components/custom/DateRangePicker";
+import {ServiceSelect} from "@components/custom/ServiceSelect";
+
+const services = ["Service A", "Service B"];
 
 export function Menu() {
   return (
     <div className="grid auto-cols-max grid-flow-col justify-end gap-5 pb-5">
+        <ServiceSelect selectedService={services[0]} services={services}/>
       <DateRangePicker />
     </div>
   );
---
+++ atlas/atlas-metrics/app/components/custom/ServiceSelect.tsx
@@ -0,0 +1,44 @@
+import {useRouter} from "next/navigation";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from "@components/ui/select";
+
+interface ServiceSelectProps {
+    selectedService: string;
+    services: string[];
+}
+
+
+export function ServiceSelect({ selectedService, services }: ServiceSelectProps) {
+    const router = useRouter();
+
+    const handleItemClick = (newService: string) => {
+        const params = new URLSearchParams(window.location.search);
+        params.set("service", newService);
+        router.push(`${window.location.pathname}?${params.toString()}`);
+    };
+
+    return (
+        <Select defaultValue={selectedService} onValueChange={(value) => handleItemClick(value)}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a service" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Service</SelectLabel>
+                    {services.map((service) => (
+                        <SelectItem key={service} value={service}>
+                            {service[0].toUpperCase() + service.slice(1)}
+                        </SelectItem>
+                    ))}
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
+}
