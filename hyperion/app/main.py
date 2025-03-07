9ace330851fee2b81e1fba5e19edec8c41b65e1e
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-03-07 20:42:12 +0100

    add demo

--- hyperion/app/main.py
+++ hyperion/app/main.py
@@ -1,6 +1,8 @@
 import gradio as gr
+from gradio.themes.utils import colors, sizes
 from fastapi import FastAPI, status
 from langfuse.callback import CallbackHandler
+from langchain_core.messages import HumanMessage, AIMessage
 
 from shared.security import AuthMiddleware, add_security_schema_to_app
 from shared.health import create_health_router
@@ -56,12 +58,84 @@ def run(query: str):
     return model.invoke(query).content
 
 
-io = gr.Interface(fn=run, inputs="textbox", outputs="textbox")
+def generate_chat_response(message, history):
+    history_langchain_format = []
+    if not history:
+        history_langchain_format.append(
+            AIMessage(
+                content=(
+                    "You are an AI assistant that provides information about TUM's courses, events, and FAQ. "
+                    "You must respond politely, professionally, and with TUM branding in mind."
+                )
+            )
+        )
+
+    for msg in history:
+        if msg["role"] == "user":
+            history_langchain_format.append(HumanMessage(content=msg["content"]))
+        elif msg["role"] == "assistant":
+            history_langchain_format.append(AIMessage(content=msg["content"]))
+    history_langchain_format.append(HumanMessage(content=message))
+    response = model.invoke(history_langchain_format)
+    return response.content
+
+tum_primary = colors.Color(
+    name="tum_primary",
+    c50="#E8F4FD",   # lightest tint (approximation)
+    c100="#C7E0FB",
+    c200="#A5CCF8",
+    c300="#82B8F5",
+    c400="#60A4F3",
+    c500="#0065BD",  # official TUM blue
+    c600="#0058AA",  # a bit darker
+    c700="#004C97",
+    c800="#004084",
+    c900="#003471",
+    c950="#00285E"
+)
+
+theme = gr.themes.Base(
+    primary_hue=tum_primary,
+    font="Arial",
+    radius_size=sizes.radius_none
+)
+
+def like(evt: gr.LikeData):
+    print("User liked the response")
+    print(evt.index, evt.liked, evt.value)
+
+hello_world = gr.Interface(fn=run, inputs="textbox", outputs="textbox")
+chat = gr.ChatInterface(
+    generate_chat_response, 
+    type="messages",
+    flagging_mode="manual",
+    flagging_options=["Like", "Dislike"],
+)
+playground = gr.TabbedInterface(
+    [hello_world, chat],
+    ["Hello World", "Chat"],
+    theme=theme,
+    css="""
+.submit-button {
+    color: white !important;
+    background-color: #3070b3 !important;
+    border-color: #3070b3 !important; 
+    border-radius: 0px; 
+    border-width: 1px !important; 
+    border-style: solid !important;
+} 
+.submit-button:hover {
+    color: #3070b3 !important;
+    background-color: white !important;
+}
+"""
+)
+
 playground_auth = (
     (settings.PLAYGROUND_USERNAME, settings.PLAYGROUND_PASSWORD)
     if settings.PLAYGROUND_PASSWORD
     else None
 )
 app = gr.mount_gradio_app(
-    app, io, path="/playground", root_path="/playground", auth=playground_auth
+    app, playground, path="/playground", root_path="/playground", auth=playground_auth
 )
