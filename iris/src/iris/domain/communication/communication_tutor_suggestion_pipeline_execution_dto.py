6249e2fcbf375f40b1c6957393a30a26b6331d7f
Alexander Joham <alexjoham@icloud.com>
2025-04-20 22:07:34 +0200

    Add text exercise pipeline to generate tutor suggestions for text exercises

--- iris/src/iris/domain/communication/communication_tutor_suggestion_pipeline_execution_dto.py
+++ iris/src/iris/domain/communication/communication_tutor_suggestion_pipeline_execution_dto.py
@@ -3,9 +3,15 @@ from typing import Optional
 from iris.domain import ChatPipelineExecutionDTO
 from iris.domain.data.course_dto import CourseDTO
 from iris.domain.data.post_dto import PostDTO
+from iris.domain.data.programming_exercise_dto import ProgrammingExerciseDTO
+from iris.domain.data.text_exercise_dto import TextExerciseDTO
 
 
 class CommunicationTutorSuggestionPipelineExecutionDTO(ChatPipelineExecutionDTO):
     course: Optional[CourseDTO] = None
     exercise_id: Optional[int] = None
+    lecture_id: Optional[int] = None
+    lecture_unit_id: Optional[int] = None
     post: PostDTO = None
+    textExerciseDTO: Optional[TextExerciseDTO] = None
+    programmingExerciseDTO: Optional[ProgrammingExerciseDTO] = None
