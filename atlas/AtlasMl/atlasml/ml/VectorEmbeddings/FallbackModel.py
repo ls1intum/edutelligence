fba5c1755ba4aab1ba367b0649dd192e2b7d4e98
Arda Karaman <arda.karaman@tum.de>
2025-05-04 13:24:38 +0200

    Add pipeline method for initial mapping

--- atlas/AtlasMl/atlasml/ml/VectorEmbeddings/FallbackModel.py
+++ atlas/AtlasMl/atlasml/ml/VectorEmbeddings/FallbackModel.py
@@ -1,9 +1,9 @@
 from sentence_transformers import SentenceTransformer
 from atlasml.clients.weaviate import weaviate_client
 
-def generate_embeddings(id: str, sentence: str):
+def generate_embeddings_local(id: str, sentence: str):
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     embeddings = model.encode(sentence)
 
     uuid = weaviate_client.add_embeddings(id, sentence, embeddings)
-    return uuid, embeddings 
+    return id, embeddings
