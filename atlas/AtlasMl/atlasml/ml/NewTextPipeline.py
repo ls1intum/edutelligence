e69a63a26b64216408b863d4ba40acb2e53a32ed
Arda Karaman <arda.karaman@tum.de>
2025-05-11 18:20:08 +0200

    - Comments

--- atlas/AtlasMl/atlasml/ml/NewTextPipeline.py
+++ atlas/AtlasMl/atlasml/ml/NewTextPipeline.py
@@ -5,6 +5,20 @@ from sklearn.metrics.pairwise import cosine_similarity
 from atlasml.ml.VectorEmbeddings.ModelDimension import ModelDimension
 
 class NewTextPipeline:
+    """
+    NewTextPipeline orchestrates text processing by generating embeddings for a single text input
+    and computing its similarity with existing cluster medoids.
+    
+    Args:
+        text (str): Input text to be embedded and processed..
+        uuid (str): Unique identifier for the input text.
+    
+    Attributes:
+        best_medoid_idx (int): Index of the most similar medoid found.
+        similarity_scores (numpy.ndarray): Array of similarity scores between input text and medoids.
+        text (str): The input text to be embedded and processed.
+        uuid (str): The unique identifier for the text.
+    """
     def __init__(self, text: str, uuid: str):
         self.best_medoid_idx = None
         self.similarity_scores = None
