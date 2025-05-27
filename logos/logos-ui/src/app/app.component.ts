b0161f6ae7951675f1ddf3345a280c6e8fa2f180
Florian Briksa <florian.briksa@tum.de>
2025-05-27 12:59:50 +0200

    Add LogosUI Stub

--- logos/logos-ui/src/app/app.component.ts
+++ logos/logos-ui/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'default';
+  title = 'logos-ui';
 }
