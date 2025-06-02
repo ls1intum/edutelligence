26be703a8aef6a85235adbc63081dcddc1f48ddd
Tim Cremer <tim.cremer@tum.de>
2025-06-02 08:22:55 +0200

    adjust prompt a bit

--- iris/src/iris/pipeline/prompts/faq_consistency_prompt.py
+++ iris/src/iris/pipeline/prompts/faq_consistency_prompt.py
@@ -10,6 +10,10 @@ For example, do not treat "exam" and "make-up exam" as identical — they refer 
 Only treat content as consistent if it refers to the same concept using either the same wording or clearly synonymous \n
 expressions within the course context. Do not assume equivalence between terms unless explicitly stated.
 
+Secondly, identify the language of the course. The language of the course is either german or english. You can extract
+the language from the existing FAQs. Your output should be in the same language as the course language.
+If you are unsure, choose english. 
+
 ### Given FAQs:
 {faqs}
 
@@ -19,15 +23,8 @@ expressions within the course context. Do not assume equivalence between terms u
 ### Output:
 
 Generate the following response dictionary:
-{
-  "type": "consistent" or "inconsistent",
-  // Additional fields below are required only if type is "inconsistent"
-  // The addition al fields are defined below
-  "faqs": [...],
-  "message": "...",
-  "suggestion": [...],
-  "improved version": "..."
-}
+"type": "consistent" or "inconsistent"
+The following four entries are optional and should only be set if inconsistencies are detected.
 
 "faqs" must be a JSON array of objects. Each entry must be a JSON dictionary with exactly the following fields:
 "faq_id" (string or number)
@@ -40,7 +37,7 @@ Include only FAQs that contradict the final_result. Do not include FAQs that are
 
 "message": "The provided text was rephrased, however it contains inconsistent information with existing FAQs."
 -Make sure to always insert two new lines after the last character of this sentences.
-The "faqs" field should contain only the inconsistent FAQs with their faq_id, faq_question_title, and faq_question_answer.
+The "faqs" field should contain only inconsistent FAQs with their faq_id, faq_question_title, and faq_question_answer.
 Make sure to not include any additional FAQs that are consistent with the final_result.
 
 -"suggestion": This entry is a list of strings, each string represents a suggestion to improve the final result.\n
