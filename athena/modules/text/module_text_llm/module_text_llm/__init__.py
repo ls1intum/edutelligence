6abcc6fc341c9e94f7eebd98a34749cbdd041d00
LeonWehrhahn <leon.wehrhahn@protonmail.ch>
2025-06-07 19:59:30 +0200

    refactor text assessment approach imports

--- athena/modules/text/module_text_llm/module_text_llm/__init__.py
+++ athena/modules/text/module_text_llm/module_text_llm/__init__.py
@@ -1,14 +1,17 @@
 import dotenv
 
-dotenv.load_dotenv(override=True)
+from .basic_approach import generate_suggestions as basic
+from .chain_of_thought_approach import generate_suggestions as chain_of_thought
+from .cot_learner_profile import generate_suggestions as cot_learner_profile
+from .divide_and_conquer import generate_suggestions as divide_and_conquer
+from .self_consistency import generate_suggestions as self_consistency
 
-# Approach Discovery
-# By importing these modules, we trigger the @register_approach decorators
-# within them, populating our central registry.
-print("Registering text assessment approaches...")
-from .basic_approach import generate_suggestions
-from .chain_of_thought_approach import generate_suggestions
-from .cot_learner_profile import generate_suggestions
-from .divide_and_conquer import generate_suggestions
-from .self_consistency import generate_suggestions
-print("Registration complete.")
+__all__ = [
+    "basic",
+    "chain_of_thought",
+    "cot_learner_profile",
+    "divide_and_conquer",
+    "self_consistency",
+]
+
+dotenv.load_dotenv(override=True)
