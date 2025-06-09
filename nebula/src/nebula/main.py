cae2307a3ccbc510c9d2a2b64c86102d812dddeb
Tim Cremer <tim.cremer@tum.de>
2025-06-09 08:48:18 +0200

    Initial setup of grpc

--- nebula/src/nebula/main.py
+++ nebula/src/nebula/main.py
@@ -1,1 +1,7 @@
-# TODO: Setup fastapi
+from nebula.services.faq_service.faq_server import serve
+
+if __name__ == "__main__":
+    server = serve()
+    print("FAQ service running on port 50051...")
+    server.start()
+    server.wait_for_termination()
---
+++ nebula/src/nebula/services/faq_service/faq_server.py
@@ -0,0 +1,14 @@
+import grpc
+from concurrent import futures
+from . import faq_pb2, faq_pb2_grpc
+
+class FAQService(faq_pb2_grpc.FAQServiceServicer):
+    def ProcessInput(self, request, context):
+        print("Received:", request.input_text)
+        return faq_pb2.FaqRewritingResponse(result="Processed: " + request.input_text)
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    faq_pb2_grpc.add_FAQServiceServicer_to_server(FAQService(), server)
+    server.add_insecure_port('[::]:50051')
+    return server
