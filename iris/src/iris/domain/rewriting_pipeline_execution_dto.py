778e89fa23267f6fd6f80d4bc4415ef9c9fa3065
Tim Cremer <tim.cremer@tum.de>
2025-06-09 08:49:00 +0200

    Linter

--- iris/src/iris/domain/rewriting_pipeline_execution_dto.py
+++ iris/src/iris/domain/rewriting_pipeline_execution_dto.py
@@ -2,11 +2,7 @@ from pydantic import BaseModel, Field
 
 from . import PipelineExecutionDTO
 
-
-
 class RewritingPipelineExecutionDTO(BaseModel):
     execution: PipelineExecutionDTO
     course_id: int = Field(alias="courseId")
     to_be_rewritten: str = Field(alias="toBeRewritten")
-
-
