1c6cd38279bbf4622465a04c0edb7bc8fd256c15
FloBenutzer <florian@briksa.org>
2025-07-05 14:23:23 +0200

    Improved token weights

--- logos/src/logos/classification/classify_ai.py
+++ logos/src/logos/classification/classify_ai.py
@@ -20,6 +20,6 @@ class AIClassifier(Classifier):
         ranking = laura.classify_prompt(prompt, top_k=len(laura.model_db))
         ranking = {idx: value for (idx, value) in ranking}
         for model in self.models:
-            model["classification_weight"].add_weight(ranking[model["id"]])
+            model["classification_weight"].add_weight(ranking[model["id"]], "ai")
             print(f"Laura weight for model {model['id']} is: {ranking[model["id"]]}")
         return self.models
