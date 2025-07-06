bf05e915ac532733e5a5d232bab5c6b67bf774c5
Alexander Joham <alexjoham@icloud.com>
2025-07-06 17:01:44 +0200

    Update prompts

--- iris/src/iris/pipeline/prompts/tutor_suggestion/text_exercise_prompt.py
+++ iris/src/iris/pipeline/prompts/tutor_suggestion/text_exercise_prompt.py
@@ -5,32 +5,23 @@ from iris.pipeline.prompts.tutor_suggestion.tutor_suggestion_final_rules import 
 
 def text_exercise_prompt():
     return (
-        """You are an assistant for a tutor that is an expert in providing suggestions how a tutor should answer student
-discussions in a thread.
-Your sole task is to generate suggestions on how to answer to the following discussion that
-has been summarized:
+        """You are an assistant for a tutor expert in guiding student discussions. 
+Your task is to generate brief suggestions for how the tutor could help students move toward the correct solution, 
+without giving away the answer. Base your response on the following:
+
 ```DISCUSSION
-{thread_summary}.
+{thread_summary}
 ```
-The discussion might be with a high probability related to the following text exercise that the students are trying to
-understand and solve, this is its problem statement:
+
+The discussion likely relates to this exercise:
 ```PROBLEM STATEMENT
-{problem_statement}. 
+{problem_statement}
 ```
-You are also allowed to look into this example solution to the exercise to understand the problem better and to help the
-tutor to answer the discussion:
+
+And this example solution:
 ```EXAMPLE SOLUTION
-{example_solution}.
+{example_solution}
 ```
-NEVER provide any solutions or steps on how to solve the exercise. Always generate suggestions that helps the tutors
-to lead the students to the right answer.
-Do not add any knowledge that is not in the problem statement or the example solution. If the question is asking for
-an explanation of a term or concept, just return the question to explain.
-Generate very short answers that helps a tutor to have an idea what to answer. Be short and
-only give short hints. Do not create full sentences! Do not tell the tutor how a student should answer the exercise.
-Tell the tutor how to help the students to go into the right direction to solve the exercise. Those suggestions should
-be hints how to help the student to find the right answer. If the answers contain direct answers to the exercise,
-remove them.
 """
         + tutor_suggestion_final_rules()
     )
