73925a6ae0cfb83cfd1ba97682f842beecced76a
Enver Can Kayandan <ge84dic@mytum.de>
2025-09-25 15:36:45 +0200

    fix attempt for playground file loading issue

--- athena/playground/src/helpers/origin_from_req.ts
+++ athena/playground/src/helpers/origin_from_req.ts
@@ -8,6 +8,8 @@ export default function getOriginFromRequest(req: NextApiRequest): string {
     protocol = forwardedProto.split(',')[0];
   } else if (Array.isArray(forwardedProto)) {
     protocol = forwardedProto[0];
+  } else if ((req.socket as any)?.encrypted) {
+    protocol = 'https';
   } else {
     protocol = 'http';
   }
