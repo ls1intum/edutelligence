47579707d83972e60b4f68cca59f00bbad584d4d
Peter Niedermoser <peter.niedermoser@live.at>
2026-01-14 00:06:21 +0100

    Iris: implemented prototype of prompt_user_agent_pipeline.py

--- iris/src/iris/web/status/status_update.py
+++ iris/src/iris/web/status/status_update.py
@@ -4,6 +4,8 @@ from threading import Lock
 from typing import Any, Optional
 
 import requests
+
+from iris.domain.chat.prompt_user_chat.prompt_user_chat_status_update_dto import PromptUserChatStatusUpdateDTO
 from memiris import Memory
 from memiris.api.memory_dto import MemoryDTO
 from sentry_sdk import capture_exception, capture_message
@@ -530,3 +532,29 @@ class AutonomousTutorCallback(StatusCallback):
             run_id,
             AutonomousTutorPipelineStatusUpdateDTO(run_state=RunStateEnum.RUNNING),
         )
+
+
+class PromptUserStatusCallback(StatusCallback):
+    """Status callback for prompt user pipelines."""
+
+    def __init__(
+            self, run_id: str, base_url: str, initial_stages: List[StageDTO] = None
+    ):
+        url = (
+            f"{base_url}/{self.api_url}/programming-exercise-prompt-user/runs/{run_id}/status"
+        )
+        current_stage_index = len(initial_stages) if initial_stages else 0
+        stages = initial_stages or []
+        stages += [
+            StageDTO(
+                weight=30,
+                state=StageStateEnum.NOT_STARTED,
+                name="Checking available information",
+            ),
+            StageDTO(
+                weight=10, state=StageStateEnum.NOT_STARTED, name="Thinking of question"
+            ),
+        ]
+        status = PromptUserChatStatusUpdateDTO(stages=stages)
+        stage = stages[current_stage_index]
+        super().__init__(url, run_id, status, stage, current_stage_index)
