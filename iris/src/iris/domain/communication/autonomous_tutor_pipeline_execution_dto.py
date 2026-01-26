4e3316929364d97e1232803aa869a72ed8f92713
Youssef El Toukhi <youssef.toukhi@tum.de>
2026-01-26 01:37:16 +0100

    Add autonomousTutorPipelineExecutionDTO

---
+++ iris/src/iris/domain/communication/autonomous_tutor_pipeline_execution_dto.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import Field
+
+from iris.domain.data.course_dto import CourseDTO
+from iris.domain.data.lecture_dto import PyrisLectureDTO
+from iris.domain.data.post_dto import PostDTO
+from iris.domain.data.programming_exercise_dto import ProgrammingExerciseDTO
+from iris.domain.data.text_exercise_dto import TextExerciseDTO
+from iris.domain.data.user_dto import UserDTO
+from iris.domain.pipeline_execution_dto import PipelineExecutionDTO
+
+
+class AutonomousTutorPipelineExecutionDto(PipelineExecutionDTO):
+    course: CourseDTO
+    post: PostDTO
+    student: UserDTO
+    programming_exercise: Optional[ProgrammingExerciseDTO] = Field(
+        default=None, alias="programmingExercise"
+    )
+    text_exercise: Optional[TextExerciseDTO] = Field(default=None, alias="textExercise")
+    lecture: Optional[PyrisLectureDTO] = Field(default=None)
+    exam_id: Optional[int] = Field(default=None, alias="examId")
