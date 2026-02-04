0b24f7e401db9efdcb0112120aed79cf93c4a7bd
Florian Briksa <92446154+flbrgit@users.noreply.github.com>
2026-02-04 19:09:20 +0100

    Iris: Add LLM Selection (#346)
    Co-authored-by: Patrick Bassner <patrick@bassner.de>

--- iris/src/iris/pipeline/session_title_generation_pipeline.py
+++ iris/src/iris/pipeline/session_title_generation_pipeline.py
@@ -26,11 +26,11 @@ class SessionTitleGenerationPipeline(SubPipeline):
     pipeline: Runnable
     tokens: TokenUsageDTO
 
-    def __init__(self):
+    def __init__(self, local: bool = False):
         super().__init__(implementation_id="session_title_generation_pipeline")
 
         # Set the langchain chat model
-        model = "gpt-4.1-nano"
+        model = "llama3.3:latest" if local else "gpt-4.1-nano"
         request_handler = ModelVersionRequestHandler(version=model)
         completion_args = CompletionArguments(temperature=0.2, max_tokens=30)
         self.llm = IrisLangchainChatModel(
--- iris/src/iris/retrieval/lecture/lecture_unit_segment_retrieval.py
+++ iris/src/iris/retrieval/lecture/lecture_unit_segment_retrieval.py
@@ -35,9 +35,11 @@ class LectureUnitSegmentRetrieval(SubPipeline):
     """LectureUnitSegmentRetrieval retrieves lecture unit segments based on search queries and returns the matching
     results."""
 
-    def __init__(self, client: WeaviateClient):
+    def __init__(self, client: WeaviateClient, local: bool = False):
         super().__init__(implementation_id="lecture_unit_segment_retrieval_pipeline")
-        request_handler = ModelVersionRequestHandler(version="gpt-4.1-mini")
+        request_handler = ModelVersionRequestHandler(
+            version="llama3.3:latest" if local else "gpt-4.1-mini"
+        )
         completion_args = CompletionArguments(temperature=0, max_tokens=2000)
         self.llm = IrisLangchainChatModel(
             request_handler=request_handler, completion_args=completion_args
