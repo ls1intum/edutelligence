ecdebbe99f6822b215e67de50f5f11e6b3d512b2
Nayer Kotry <83802319+Nayer-kotry@users.noreply.github.com>
2026-02-23 22:44:26 +0100

    add lecture search endpoint

---
+++ iris/src/iris/web/routers/search.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter
+from fastapi.params import Depends
+
+from iris.common.logging_config import get_logger
+from iris.dependencies import TokenValidator
+from iris.domain.search.lecture_search_dto import (
+    LectureSearchRequestDTO,
+    LectureSearchResultDTO,
+)
+from iris.retrieval.lecture.lecture_global_search_retrieval import (
+    LectureGlobalSearchRetrieval,
+)
+from iris.vector_database.database import VectorDatabase
+
+router = APIRouter(prefix="/api/v1/search", tags=["search"])
+logger = get_logger(__name__)
+
+
+@router.get("/lectures", dependencies=[Depends(TokenValidator())])
+def lecture_search(dto: LectureSearchRequestDTO) -> list[LectureSearchResultDTO]:
+    """
+    Search for lectures based on a query.
+
+    :return: The search results.
+    """
+    client = VectorDatabase().get_client()
+    return LectureGlobalSearchRetrieval(client).search(dto.query, dto.limit)
