812d3c4c8c84353ced40b2c0c51eb3b91ec05ad9
Senan Aslan <senanaslan29@gmail.com>
2026-05-08 22:14:52 +0200

    Revert "fix: add local attribute support to LectureRetrieval, FaqRetrieval and embedding_model"
    This reverts commit 639470eb6780f1045810121d6dcda3d5fef9966c.

--- iris/src/iris/retrieval/lecture/lecture_retrieval.py
+++ iris/src/iris/retrieval/lecture/lecture_retrieval.py
@@ -87,7 +87,7 @@ class LectureRetrieval(SubPipeline):
         pipeline_id = "lecture_retrieval_pipeline"
         chat_model = resolve_model(pipeline_id, "default", "chat", local=local)
         embedding_model = resolve_model(
-            pipeline_id, "default", "embedding", local=local
+            pipeline_id, "default", "embedding", local=False
         )
         request_handler = LlmRequestHandler(model_id=chat_model)
         completion_args = CompletionArguments(temperature=0, max_tokens=2000)
