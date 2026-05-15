532ae7f21a7dd380b2658351c199e205df46e3e4
Maximilian Soelch <maximilian.soelch@tum.de>
2026-05-15 10:17:53 +0200

    Change import to fix linter warning

--- athena/modules/text/module_text_llm/module_text_llm/config.py
+++ athena/modules/text/module_text_llm/module_text_llm/config.py
@@ -6,7 +6,7 @@ from athena import config_schema_provider
 from module_text_llm.default_approach import DefaultApproachConfig
 from module_text_llm.divide_and_conquer import DivideAndConquerConfig
 from module_text_llm.self_consistency import SelfConsistencyConfig
-import module_text_llm.approach_config as approach_config
+from module_text_llm import approach_config
 
 llm_config = approach_config.llm_config
 
