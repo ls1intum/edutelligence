fc3a6de2bf791abbc5db2688042d1f2b1df1abfc
Maximilian Soelch <maximilian.soelch@tum.de>
2026-05-15 09:43:46 +0200

    Import llm_config so text llm is considered a llm module

--- athena/modules/text/module_text_llm/module_text_llm/config.py
+++ athena/modules/text/module_text_llm/module_text_llm/config.py
@@ -6,6 +6,9 @@ from athena import config_schema_provider
 from module_text_llm.default_approach import DefaultApproachConfig
 from module_text_llm.divide_and_conquer import DivideAndConquerConfig
 from module_text_llm.self_consistency import SelfConsistencyConfig
+import module_text_llm.approach_config as approach_config
+
+llm_config = approach_config.llm_config
 
 ApproachConfigUnion = Union[DefaultApproachConfig, DivideAndConquerConfig, SelfConsistencyConfig]
 
